f5cd0a4b9d9560d181cca5493a1175fac037ac5d
Julio Leme <jleme654@gmail.com>
2017-12-20 11:53:02 -0200

    versao com testes - v2

--- src/br/com/desafio/infoglobo/MainCrawlerTest.java
+++ src/br/com/desafio/infoglobo/MainCrawlerTest.java
@@ -1,33 +1,33 @@
-package br.com.desafio.infoglobo;
-
-import static org.junit.Assert.fail;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import br.com.desafio.infoglobo.model.Feed;
-
-public class MainCrawlerTest {
-
-	@InjectMocks
-	MainCrawler maintest;
-
-	@Before
-	public void init() {
-		MockitoAnnotations.initMocks(this);
-
-	}
-
-	@Test
-	public void testLeWebservice() {
-		try {
-			Mockito.when(this.maintest.leWebservice()).thenReturn(new Feed());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-}
+//package br.com.desafio.infoglobo;
+//
+//import static org.junit.Assert.fail;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mockito;
+//import org.mockito.MockitoAnnotations;
+//
+//import br.com.desafio.infoglobo.model.Feed;
+//
+//public class MainCrawlerTest {
+//
+//	/*@InjectMocks
+//	MainCrawler maintest;
+//
+//	@Before
+//	public void init() {
+//		MockitoAnnotations.initMocks(this);
+//
+//	}
+//
+//	@Test
+//	public void testLeWebservice() {
+//		try {
+//			Mockito.when(this.maintest.leWebservice()).thenReturn(new Feed());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}*/
+//
+//}
